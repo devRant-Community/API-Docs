e29933696467610305522fb4bf7ad9233c8e5482
Skayo <jonasg.cool@gmail.com>
2018-01-25 23:32:13 +0100

    Added some kind of CMS-System and other stuff
    Holy fuck I don't keep track of what I change help

--- app/controllers/docs.js
+++ app/controllers/docs.js
@@ -1,20 +1,60 @@
-app.controller('DocsController', function ($scope, $location) {
-	if(localStorage.getItem('examples-language') === null) {
-		$scope.language = 'php';
-		localStorage.setItem('examples-language', $scope.language);
-	} else {
-		$scope.language = localStorage.getItem('examples-language');
-		$('.code-language-option.active').removeClass('active');
-		$('.language-' + $scope.language).addClass('active');
-	}
-
-	$scope.setLanguage = function(lang) {
-		if(lang == $scope.language)
+app.controller('DocsController', function ($scope, $http, $timeout) {
+	$scope.setLanguage = function (lang) {console.log(lang);
+		if (lang === $scope.language) {
 			return;
+		}
 
 		$('.language-' + $scope.language).removeClass('active');
 		$scope.language = lang;
 		localStorage.setItem('examples-language', $scope.language);
 		$('.language-' + $scope.language).addClass('active');
 	};
+
+	
+	$scope.getExampleCode = function(endpoint, lang) {
+		console.log(endpoint, lang);
+		return endpoint.exampleCode[lang];
+	};
+
+
+	$http.get('content/docs/endpoints.json').then(function (response) {
+		$scope.endpoints = response.data;
+		$scope.endpointsList = [];
+
+		$scope.endpoints.forEach(function (endpoint, i) {
+			$scope.endpoints[i].langs = [];
+
+			endpoint.exampleCode.forEach(function (lang, j) {
+				$scope.endpoints[i].exampleCode[lang.langCode] = lang.code.join('\n');
+
+				$scope.endpoints[i].langs.push({
+					                            name: lang.name, langCode: lang.langCode
+				                            });
+			});
+
+			$scope.endpoints[i].exampleResponse = endpoint.exampleResponse.join('\n');
+
+			$scope.endpointsList.push(endpoint.endpoint)
+		});
+
+		$scope.endpoints.sort(function(a,b) {
+			if (a.endpoint < b.endpoint)
+				return -1;
+			if (a.endpoint > b.endpoint)
+				return 1;
+			return 0;
+		});
+	});
+
+
+	$timeout(function () {
+		if (localStorage.getItem('examples-language') === null) {
+			$scope.language = 'curl';
+			localStorage.setItem('examples-language', $scope.language);
+		} else {
+			$scope.language = localStorage.getItem('examples-language');
+			$('.code-language-option.active').removeClass('active');
+			$('.language-' + $scope.language).addClass('active');
+		}
+	});
 });
--- app/controllers/playground.js
+++ app/controllers/playground.js
@@ -53,24 +53,4 @@ app.controller('TestingAreaController', function ($scope, $http, $sce) {
 			});
 		}
 	};
-
-	/*
-	 $.ajax({
-	 type: "POST",
-	 url: 'contact',
-	 dataType: 'json',
-	 cache: false,
-	 data: $('#contact-form').serialize(),
-	 success: function(data) {
-
-	 if(data.info !== 'error'){
-	 $this.parents('form').find('input[type=text],input[type=email],textarea,select').filter(':visible').val('');
-	 message.hide().removeClass('success').removeClass('error').addClass('success').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
-	 window.grecaptcha.reset();
-	 } else {
-	 message.hide().removeClass('success').removeClass('error').addClass('error').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
-	 }
-	 }
-	 });
-	 */
 });
