a220d207e54a760612ec22eb7e15c93037dca0fe
Skayo <jonasg.cool@gmail.com>
2018-01-31 18:22:32 +0100

    Changed API Domain again (WTF GitHub?)

--- app/app.js
+++ app/app.js
@@ -44,4 +44,4 @@ app.config(['$routeProvider', '$locationProvider', '$sceProvider', '$httpProvide
 ]);
 
 // Constants
-const API = 'https://skayo.lima-city.at';
+const API = 'https://skayo.lima-city.de';
